f9b98d9a5a247c925e5f9761ff2ab14e3d2a941c
Florian Popa <fpopa1991@gmail.com>
2023-12-05 12:03:00 +0200

    Update cashcards

--- src/main/java/com/webgenerals/springacademy/controller/CashCardController.java
+++ src/main/java/com/webgenerals/springacademy/controller/CashCardController.java
@@ -36,11 +36,11 @@ public class CashCardController {
 	}
 
 	@GetMapping("/{requestedId}")
-	public ResponseEntity<CashCard> findById(@PathVariable Long requestedId, Principal principal) {
-		Optional<CashCard> cashCardOptional = Optional.ofNullable(cashCardRepository.findByIdAndOwner(requestedId, principal.getName()));
+	private ResponseEntity<CashCard> findById(@PathVariable Long requestedId, Principal principal) {
+		CashCard cashCard = findCashCard(requestedId, principal);
 
-		if (cashCardOptional.isPresent()) {
-			return ResponseEntity.ok(cashCardOptional.get());
+		if (cashCard != null) {
+			return ResponseEntity.ok(cashCard);
 		} else {
 			return ResponseEntity.notFound().build();
 		}
@@ -53,6 +53,23 @@ public class CashCardController {
 		URI locationOfNewCashCard = ucb.path("cashcards/{id}").buildAndExpand(savedCashCard.getId()).toUri();
 
 		return ResponseEntity.created(locationOfNewCashCard).build();
+	}
+
+	@PutMapping("/{requestedId}")
+	private ResponseEntity<Void> putCashCard(@PathVariable Long requestedId, @RequestBody CashCard cashCardUpdate, Principal principal) {
+		CashCard cashCard = findCashCard(requestedId, principal);
+
+		if (cashCard != null) {
+			CashCard updatedCashCard = new CashCard(cashCard.getId(), cashCardUpdate.getAmount(), principal.getName());
+			cashCardRepository.save(updatedCashCard);
+			return ResponseEntity.noContent().build();
+		}
+
+		return ResponseEntity.notFound().build();
+	}
+
+	private CashCard findCashCard(Long requestedId, Principal principal) {
+		return cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
 	}
 
 }
